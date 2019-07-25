cf5bfe1cafbba853dd658160c2d53db343c04744
Shrilakshmi Bonageri <shrisb@uw.edu>
2019-07-25 11:41:34 -0700

    Updated the version in setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@
 from setuptools import setup
 
 setup(name = 'musicalrobot',
-    version = '0.4',
+    version = '0.5',
     packages = ['musicalrobot'],
     url = 'https://github.com/pozzocapstone/musical-robot',
     license = 'MIT',
